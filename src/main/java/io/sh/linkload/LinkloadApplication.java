97cf15a4274aa160f666e87f4ffd7c3e99b2e3cf
samirrh <samirhaque808@gmail.com>
2021-11-02 23:33:21 -0400

    initial

---
+++ src/main/java/io/sh/linkload/LinkloadApplication.java
@@ -0,0 +1,13 @@
+package io.sh.linkload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkloadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/sh/linkload/LinkloadApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sh.linkload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
