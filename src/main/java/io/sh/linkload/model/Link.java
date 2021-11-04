d1e6e60641cc3ef2abbc73f0caba443ab5f5af96
samirrh <samirhaque808@gmail.com>
2021-11-03 20:31:08 -0400

    entity model

---
+++ src/main/java/io/sh/linkload/model/Link.java
@@ -0,0 +1,41 @@
+package io.sh.linkload.model;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.lang.Nullable;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Link {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long linkId;
+    @NotBlank(message = "Link name cannot be empty or null")
+    private String linkName;
+    @NotBlank(message = "URL cannot be empty or null")
+    private String url;
+    @Nullable
+    @Lob
+    private String description;
+    private Integer visits;
+    private Boolean showVisits;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+}
---
+++ src/main/java/io/sh/linkload/model/User.java
@@ -0,0 +1,31 @@
+package io.sh.linkload.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long userId;
+    @NotBlank(message = "Username required")
+    private String username;
+    @NotBlank(message = "Password required")
+    private String password;
+    @Email
+    @NotEmpty(message = "Email required")
+    private String email;
+    private boolean enabled;
+}
