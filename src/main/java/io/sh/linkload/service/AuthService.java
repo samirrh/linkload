0713e93725878d05817ceff7cb132f19f8919667
samirrh <samirhaque808@gmail.com>
2021-11-04 23:26:22 -0400

    verification

--- src/main/java/io/sh/linkload/service/AuthService.java
+++ src/main/java/io/sh/linkload/service/AuthService.java
@@ -1,17 +1,45 @@
 package io.sh.linkload.service;
 
+import java.util.UUID;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import io.sh.linkload.dto.RegisterRequest;
 import io.sh.linkload.model.User;
+import io.sh.linkload.model.VerificationToken;
+import io.sh.linkload.repository.UserRepository;
+import io.sh.linkload.repository.VerificationTokenRepository;
 
 @Service
 public class AuthService {
+    // maybe put autowired
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final VerificationTokenRepository verificationTokenRepository;
+
+    @Transactional
     public void signup(RegisterRequest registerRequest) {
         User user = new User();
         user.setUsername(registerRequest.getUsername());
         user.setEmail(registerRequest.getEmail());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
         user.setVerified(false);
+
+        userRepository.save(user);
+        String token = generateVerificationToken(user);
+    }
+
+    private String generateVerificationToken(User user) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+
+        verificationTokenRepository.save(verificationToken);
+        return token;
     }
 }
