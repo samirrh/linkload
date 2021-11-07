2ec18cb4cf03a8ce69c965d4786f9d406462bee5
samirrh <samirhaque808@gmail.com>
2021-11-06 23:09:41 -0400

    async sending email

--- src/main/java/io/sh/linkload/service/EmailService.java
+++ src/main/java/io/sh/linkload/service/EmailService.java
@@ -4,6 +4,7 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import io.sh.linkload.model.NotificationEmail;
@@ -18,6 +19,7 @@ public class EmailService {
     private final JavaMailSender mailSender;
     private final EmailGenerator emailGenerator;
 
+    @Async
     void sendMail(NotificationEmail notificationEmail) {
         MimeMessagePreparator messagePreparator = mimeMessage -> {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
